9a9fe32d34f7ea312b20dec592cb23bf13d905f6
18862216627 <18862216627@163.com>
2021-01-13 00:43:46 +0800

    登录注册

---
+++ assets/js/login.js
@@ -0,0 +1,66 @@
+$(function() {
+    $('#link_reg').on('click', function() {
+        $('.login-box').hide()
+        $('.reg-box').show()
+    })
+    $('#link_login').on('click', function() {
+        $('.login-box').show()
+        $('.reg-box').hide()
+    });
+
+    var form = layui.form
+    var layer = layui.layer
+    form.verify({
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        repwd: function(value) {
+            var pwd = $('.reg-box [name=password]').val()
+            if (pwd !== value) {
+                return '两次密码不一致！'
+            }
+        }
+    })
+    $('#form_reg').on('submit', function(e) {
+            e.preventDefault()
+            $.ajax({
+                type: 'post',
+                url: '/api/reguser',
+                data: {
+                    username: $('#form_reg [name=username]').val(),
+                    password: $('#form_reg [name=password]').val()
+                },
+                // dataType: 'dataType',
+                success: function(res) {
+                    if (res.status !== 0) {
+                        return console.log(res.message);
+                    } else {
+                        return console.log('注册成功');
+                    }
+                }
+
+            });
+            layer.msg('注册成功')
+            $('#link_login').click();
+        })
+        // 监听登录表单的提交事件
+    $('#form_login').submit(function(e) {
+        // 阻止默认提交行为
+        e.preventDefault()
+        $.ajax({
+            url: '/api/login',
+            method: 'POST',
+            // 快速获取表单中的数据
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败！')
+                }
+                layer.msg('登录成功！')
+                    // 将登录成功得到的 token 字符串，保存到 localStorage 中
+                localStorage.setItem('token', res.token)
+                    // 跳转到后台主页
+                location.href = '/index.html'
+            }
+        })
+    })
+
+})
